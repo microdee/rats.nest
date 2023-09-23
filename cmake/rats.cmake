d19018535f6c484aa232c716587cd1128711f477
David Mórász <m@mcro.de>
2023-09-24 01:07:08 +0200

    fix cmake issues with rats project model

--- cmake/rats.cmake
+++ cmake/rats.cmake
@@ -2,6 +2,4 @@
     Includes all functionalities for building with the rats ecosystem
 ]]#
 
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH};${CMAKE_CURRENT_SOURCE_DIR} CACHE "" FORCE)
-
-include(rats.packs.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/rats.packs.cmake)
---
+++ packs/tests/rats.cmake_tests/check_directories/directories.cmake
@@ -0,0 +1,4 @@
+
+message(STATUS "Source Dir: ${CMAKE_SOURCE_DIR}")
+message(STATUS "Curr List Dir: ${CMAKE_CURRENT_LIST_DIR}")
+message(STATUS "Curr Source Dir: ${CMAKE_CURRENT_SOURCE_DIR}")
