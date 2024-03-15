cc390d05fa296d47ca8fb6e37d1933e466cc40d0
David Mórász <david.morasz@realworld-one.com>
2024-03-15 20:01:21 +0100

    pretending with stringzilla

--- packs/rats/core/base/src/public/string/extensions.h
+++ packs/rats/core/base/src/public/string/extensions.h
@@ -11,33 +11,18 @@ namespace rats::core::base::string::extensions
     using namespace rats::core::base::string::traits;
     using namespace rats::core::base::language::extension_methods;
 
-    template <StringOrView String>
-    constexpr std::string operator / (const String& ls, const String& rs)
+    namespace sz = ashvardanian::stringzilla;
+    using namespace sz::literals;
+
+    template <StringOrView LsString, StringOrView RsString>
+    auto operator / (LsString&& ls, RsString&& rs)
     {
-        return ls + "/" + rs;
+        return sz::concatenate(std::forward(ls), "/"_sz, std::forward(rs));
     }
 
-    template <StringOrView String>
-    constexpr std::string operator / (String&& ls, String&& rs)
+    template <StringOrView String, typename... ConcatenationArgs>
+    auto operator / (sz::concatenation<ConcatenationArgs...>&& ls, String&& rs)
     {
-        return ls + "/" + rs;
-    }
-
-    struct TakeUntil
-    {
-        using Predicate = std::move_only_function<bool, int>
-        private: 
-    }
-
-    struct Trim
-    {
-        private: char TrimChar;
-        public: Trim(char trimChar) : TrimChar(trimChar) {}
-
-        template <StringOrView String>
-        std::string_view operator () (const String& ls) const
-        {
-
-        }
+        return ls | "/"_sz | rs;
     }
 }
--- packs/rats/core/base/src/public/string/traits.h
+++ packs/rats/core/base/src/public/string/traits.h
@@ -2,8 +2,18 @@
 
 #include <string>
 
+#include "stringzilla.hpp"
+
 namespace rats::core::base::string::traits
 {
+    namespace sz = ashvardanian::stringzilla;
+    
     template <typename T>
-    concept StringOrView = std::same_as<T, std::string> || std::same_as<T, std::string_view>;
+    concept StdStringOrView = std::same_as<T, std::string> || std::same_as<T, std::string_view>;
+
+    template <typename T>
+    concept SzStringOrView = std::same_as<T, sz::string> || std::same_as<T, sz::string_view>;
+
+    template <typename T>
+    concept StringOrView = StdStringOrView<T> || SzStringOrView<T>;
 }
