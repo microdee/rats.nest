8de3efe65f523eddb6853823c71c36be71325677
David Mórász <david.morasz@realworld-one.com>
2024-07-09 17:54:20 +0200

    + imgui tpjn + references field in tpjn

--- packs/rats/core/imgui/xmake.lua
+++ packs/rats/core/imgui/xmake.lua
@@ -3,6 +3,16 @@ add_rules("mode.debug", "mode.release")
 local ns = NS.use()
 local ns_cpp = NS.use("rats.xmake.cpp")
 
+--[[ yaml
+third-party:
+    name: Dear ImGui
+    source: https://github.com/ocornut/imgui
+    project: https://dearimgui.com
+    authors:
+        - "Omar Cornut (https://github.com/ocornut, https://www.miracleworld.net)"
+    license: MIT License (https://github.com/ocornut/imgui/blob/master/LICENSE.txt)
+    reasoning: The best ever tools UI library.
+]]
 add_requires("imgui v1.90.2-docking", {
     configs = {
         shared = true,
